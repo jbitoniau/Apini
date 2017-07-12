a7d4333e22994c972692b829de1d1cda522a0fcd
jbitoniau <jbitoniau@gmail.com>
2017-07-12 14:20:09 +0200

    Updated TelemetryServer so it takes care of providing telemetry data to the TelemetrySender (rather than this latter directly connecting itself to the TelemetryReceiver)

--- TelemetryServer/TelemetryServer.js
+++ TelemetryServer/TelemetryServer.js
@@ -11,161 +11,180 @@ var telemetryReceiverMod = require('./server/TelemetryReceiver');
 var telemetrySenderMod = require('./server/TelemetrySender');
 
 /*
-	TelemetryServer
+    TelemetryServer
 */
 function TelemetryServer() {
-	// Create TelemetryReceiver working on the UDP socket
-	this._telemetryReceiver = new telemetryReceiverMod.TelemetryReceiver();
+    // Create TelemetryReceiver working on the UDP socket
+    this._telemetryReceiver = new telemetryReceiverMod.TelemetryReceiver();
 
-	// Prepare TelemetrySenders array for incoming websocket connections
-	this._telemetrySenders = [];
+    // Prepare TelemetrySenders array for incoming websocket connections
+    this._telemetrySenders = [];
 
-	// Create HTTP server
-	this._httpServer = http.createServer(function(req, res) {
-		var path = url.parse(req.url).pathname;
-		var query = url.parse(req.url).query;
-		if (path.indexOf('/') === 0) {
-			var filename = path.substr(1);
-			if (filename.length === 0) {
-				filename = 'TelemetryViewer.html';
-			}
-			TelemetryServer._serveFile(filename, res);
-		} else {
-			TelemetryServer._createHTMLErrorResponse(res, 404, 'Page not found');
-		}
-	});
-	this._httpServer.listen(8080, function() {
-		console.log('HTTP server listening');
-	});
+    // Create HTTP server
+    this._httpServer = http.createServer(function(req, res) {
+        var path = url.parse(req.url).pathname;
+        var query = url.parse(req.url).query;
+        if (path.indexOf('/') === 0) {
+            var filename = path.substr(1);
+            if (filename.length === 0) {
+                filename = 'TelemetryViewer.html';
+            }
+            TelemetryServer._serveFile(filename, res);
+        } else {
+            TelemetryServer._createHTMLErrorResponse(res, 404, 'Page not found');
+        }
+    });
+    this._httpServer.listen(8080, function() {
+        console.log('HTTP server listening');
+    });
 
-	// Create Websocket server
-	this._websocketServer = new websocket.server({
-		httpServer: this._httpServer
-	});
-	this._websocketServer.on(
-		'request',
-		function(request) {
-			var websocketConnection = request.accept(null, request.origin);
+    // Create Websocket server
+    this._websocketServer = new websocket.server({
+        httpServer: this._httpServer
+    });
+    this._websocketServer.on(
+        'request',
+        function(request) {
+            var websocketConnection = request.accept(null, request.origin);
 
-			// TEMP! TEMP!
-			// websocketConnectionelemetrySamplesToSend.on('message', function(message) {
-			// 	console.log('Received ' + message.type + ' message:' + message.utf8Data);
-			// });
-			// TEMP! TEMP!
+            // TEMP! TEMP!
+            // websocketConnectionelemetrySamplesToSend.on('message', function(message) {
+            //  console.log('Received ' + message.type + ' message:' + message.utf8Data);
+            // });
+            // TEMP! TEMP!
 
-			var telemetrySender = new telemetrySenderMod.TelemetrySender(this._telemetryReceiver, websocketConnection);
-			this._telemetrySenders.push(telemetrySender);
+            // Create a new TelemetrySender for this websocket connection
+            var telemetrySender = new telemetrySenderMod.TelemetrySender(websocketConnection);
 
-			console.log('TelemetryServer: ' + this._telemetrySenders.length + ' connections in progress');
+            // Whenever we get a new telemetry sample from the TelemetryReceiver, give it to the TelemetrySender for sending
+            var onTelemetrySampleReadyHandler = function(telemetrySample) {
+                telemetrySender.addTelemetrySampleToSend(telemetrySample);
+            };
+            this._telemetryReceiver._onTelemetrySampleReadyListeners.push(onTelemetrySampleReadyHandler);
 
-			websocketConnection.on(
-				'close',
-				function(websocketConnection) {
-					//console.log('Websocket server connection closed' + websocketConnection);
-					telemetrySender.dispose();
-					var index = this._telemetrySenders.indexOf(telemetrySender);
-					if (index !== -1) {
-						this._telemetrySenders.splice(index, 1);
-					} else {
-						console.warn("couldn't find sender for connection...");
-					}
+            // Remember this TelemetrySender
+            this._telemetrySenders.push(telemetrySender);
 
-					console.log('TelemetryServer: ' + this._telemetrySenders.length + ' connections in progress');
-				}.bind(this)
-			);
-		}.bind(this)
-	);
-	console.log('Websocket server created');
+            console.log('TelemetryServer: ' + this._telemetrySenders.length + ' connections in progress');
+
+            websocketConnection.on(
+                'close',
+                function(websocketConnection) {
+                    // Stop forwarding telemetry samples from TelemetryReceiver to TelemetrySender
+                    var index = this._telemetryReceiver._onTelemetrySampleReadyListeners.indexOf(onTelemetrySampleReadyHandler);
+                    if (index !== -1) {
+                        this._telemetryReceiver._onTelemetrySampleReadyListeners.splice(index, 1);
+                    } else {
+                        console.warn("TelemetryServer: couldn't find TelemetrySender's handler in TelemetryReceiver's listeners");
+                    }
+
+                    // Forget about this TelemetrySender
+                    var index = this._telemetrySenders.indexOf(telemetrySender);
+                    if (index !== -1) {
+                        this._telemetrySenders.splice(index, 1);
+                    } else {
+                        console.warn("TelemetryServer: couldn't find TelemetrySender corresponding to closing connection");
+                    }
+
+                    // Dispose TelemetrySender
+                    telemetrySender.dispose();
+
+                    console.log('TelemetryServer: ' + this._telemetrySenders.length + ' connections in progress');
+                }.bind(this)
+            );
+        }.bind(this)
+    );
+    console.log('Websocket server created');
 }
 
 TelemetryServer.prototype.dispose = function() {
-	// Stop things here!
-	console.log('dispose to implement here...');
+    // Stop things here!
+    console.log('dispose to implement here...');
 };
 
 TelemetryServer._getFilenameExtension = function(filename) {
-	var parts = filename.split('.');
-	if (parts.length > 1) {
-		return parts[parts.length - 1];
-	}
-	return '';
+    var parts = filename.split('.');
+    if (parts.length > 1) {
+        return parts[parts.length - 1];
+    }
+    return '';
 };
 
 TelemetryServer._getFileContentType = function(filename) {
-	var contentType = null;
-	var extension = TelemetryServer._getFilenameExtension(filename).toLowerCase();
-	switch (extension) {
-		case 'html':
-			return 'text/html';
-		case 'js':
-			return 'application/javascript';
-	}
-	return null;
+    var contentType = null;
+    var extension = TelemetryServer._getFilenameExtension(filename).toLowerCase();
+    switch (extension) {
+        case 'html':
+            return 'text/html';
+        case 'js':
+            return 'application/javascript';
+    }
+    return null;
 };
 
 TelemetryServer._serveFile = function(filename, res) {
-	var contentType = TelemetryServer._getFileContentType(filename);
-	if (!contentType) {
-		console.warn('Serving file: ' + filename + '. Unsupported file/content type');
-		res.end();
-		return;
-	}
-	console.log('Serving file: ' + filename + ' as ' + contentType);
+    var contentType = TelemetryServer._getFileContentType(filename);
+    if (!contentType) {
+        console.warn('Serving file: ' + filename + '. Unsupported file/content type');
+        res.end();
+        return;
+    }
+    console.log('Serving file: ' + filename + ' as ' + contentType);
 
-	filename = 'client/' + filename;
+    filename = 'client/' + filename;
 
-	fs.readFile(filename, 'utf8', function(err, data) {
-		if (err) {
-			TelemetryServer._createHTMLErrorResponse(res, 500, err);
-		} else {
-			res.writeHead(200, { 'content-type': contentType });
-			res.write(data);
-			res.end();
-		}
-	});
+    fs.readFile(filename, 'utf8', function(err, data) {
+        if (err) {
+            TelemetryServer._createHTMLErrorResponse(res, 500, err);
+        } else {
+            res.writeHead(200, { 'content-type': contentType });
+            res.write(data);
+            res.end();
+        }
+    });
 };
 
 TelemetryServer._createHTMLErrorResponse = function(res, code, message) {
-	res.writeHead(code, { 'content-type': 'text/html' });
-	res.write(
-		'<!DOCTYPE html>' +
-			'<html>' +
-			'    <head>' +
-			'        <meta charset="utf-8" />' +
-			'        <title>Error</title>' +
-			'    </head>' +
-			'    <body>' +
-			'     	<p>' +
-			message +
-			'</p>' +
-			'    </body>' +
-			'</html>'
-	);
-	res.end();
+    res.writeHead(code, { 'content-type': 'text/html' });
+    res.write(
+        '<!DOCTYPE html>' +
+            '<html>' +
+            '    <head>' +
+            '        <meta charset="utf-8" />' +
+            '        <title>Error</title>' +
+            '    </head>' +
+            '    <body>' +
+            '       <p>' +
+            message +
+            '</p>' +
+            '    </body>' +
+            '</html>'
+    );
+    res.end();
 };
 
 /*
-	Main
+    Main
 */
 function Main() {
-	var telemetryServer = new TelemetryServer();
+    var telemetryServer = new TelemetryServer();
 
-	//http://stackoverflow.com/questions/10021373/what-is-the-windows-equivalent-of-process-onsigint-in-node-js/14861513#14861513
-	//http://stackoverflow.com/questions/6958780/quitting-node-js-gracefully
-	if (process.platform === 'win32') {
-		var rl = require('readline').createInterface({
-			input: process.stdin,
-			output: process.stdout
-		});
-		rl.on('SIGINT', function() {
-			process.emit('SIGINT');
-		});
-	}
-	process.on('SIGINT', function() {
-		console.log('Stopping server...');
-		telemetryServer.dispose();
-		process.exit();
-	});
+    //http://stackoverflow.com/questions/10021373/what-is-the-windows-equivalent-of-process-onsigint-in-node-js/14861513#14861513
+    //http://stackoverflow.com/questions/6958780/quitting-node-js-gracefully
+    if (process.platform === 'win32') {
+        var rl = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        rl.on('SIGINT', function() {
+            process.emit('SIGINT');
+        });
+    }
+    process.on('SIGINT', function() {
+        console.log('Stopping server...');
+        telemetryServer.dispose();
+        process.exit();
+    });
 }
 
 Main();
--- TelemetryServer/server/TelemetrySender.js
+++ TelemetryServer/server/TelemetrySender.js
@@ -1,24 +1,16 @@
 /*
     TelemetrySender
-
-    A TelemetrySender pairs a TelemetryReceiver to a websocket connection.
-
-    Whenever a telemetry sample is ready from the TelemetryReceiver, this object stores it. 
-    
-    Then on a regular basis, it sends the accumulated telemetry samples on the websocket. 
-    Packaging samples together like this is better for the connection.
 */
-var TelemetrySender = function(telemetryReceiver, websocketConnection) {
-    this._telemetryReceiver = telemetryReceiver;
+var TelemetrySender = function(websocketConnection) {
     this._websocketConnection = websocketConnection;
 
-    this._interval = setInterval(this._sendData.bind(this), 50);
+    this._interval = setInterval(this.send.bind(this), 50);
 
     this._maxNumTelemetrySamplesToSend = 100;
     this._telemetrySamplesToSend = [];
 
-    this._onTelemetrySampleReadyHandler = this._onTelemetrySampleReady.bind(this);
-    this._telemetryReceiver._onTelemetrySampleReadyListeners.push(this._onTelemetrySampleReadyHandler);
+    // this._onTelemetrySampleReadyHandler = this._onTelemetrySampleReady.bind(this);
+    // this._telemetryReceiver._onTelemetrySampleReadyListeners.push(this._onTelemetrySampleReadyHandler);
 
     this._instanceID = TelemetrySender.numInstances; // Just for debug
     TelemetrySender.numInstances++;
@@ -29,27 +21,21 @@ TelemetrySender.numInstances = 0;
 
 TelemetrySender.prototype.dispose = function() {
     console.log('TelemetrySender #' + this._instanceID + ': dispose');
-
     clearInterval(this._interval);
-
-    var index = this._telemetryReceiver._onTelemetrySampleReadyListeners.indexOf(this._onTelemetrySampleReadyHandler);
-    if (index !== -1) {
-        this._telemetryReceiver._onTelemetrySampleReadyListeners.splice(index, 1);
-        TelemetrySender.numInstances--;
-    } else {
-        console.error("something's wrong");
-    }
+    TelemetrySender.numInstances--;
 };
 
-TelemetrySender.prototype._onTelemetrySampleReady = function(telemetrySample) {
+TelemetrySender.prototype.addTelemetrySampleToSend = function(telemetrySample)
+{
+    // TODO: check state of websocketConnection here...
     this._telemetrySamplesToSend.splice(0, 0, telemetrySample);
     if (this._telemetrySamplesToSend.length > this._maxNumTelemetrySamplesToSend) {
         this._telemetrySamplesToSend.shift();
-        console.warn('buffer full');
+        console.warn('TelemetrySender.addTelemetrySampleToSend: buffer is full, dropping oldest telemetry sample');
     }
 };
 
-TelemetrySender.prototype._sendData = function() {
+TelemetrySender.prototype.send = function() {
     //console.log("TelemetrySender#"+ this._instanceID + ": sending " + this._telemetrySamplesToSend.length);
     var jsonData = JSON.stringify(this._telemetrySamplesToSend);
     this._websocketConnection.sendUTF(jsonData);
