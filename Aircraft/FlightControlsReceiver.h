d61b7af0d7f62ca21c55475a9575cb00c840359c
jbitoniau <jbitoniau@gmail.com>
2017-10-10 13:26:24 +0200

    Fixed Aircraft FlightControlsReceived now that we send control packets more often

--- Aircraft/FlightControlsReceiver.h
+++ Aircraft/FlightControlsReceiver.h
@@ -15,7 +15,7 @@ private:
     static bool deserializeFlightControls( char* messageBuffer, unsigned int messageSize, FlightControls& flightControls );
 
     Loco::UDPSocket* socket;
-    const int bufferSize = 4*4;
+    const int bufferSize = 4*4  *2;  // x2 because the server sends a flight packet every 10ms and we receive only every 20ms
     char* buffer;
 
     const unsigned int localPort = 8081;
